8df81b58365921218de5ec6dcc331c8b17044119
user <ismdevteam@localhost>
2026-02-03 15:12:06 +0000

    re-added examples from commit df58f5b7f6a4ed283f4f83132a50cdd004969031

--- examples/client_test_piece_cache_alerts.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <libtorrent/session.hpp>
-#include <libtorrent/alert_types.hpp>
-
-struct client_state_t;
-
-bool handle_alert(client_state_t& client_state, lt::alert* a);
-void pop_alerts(client_state_t& client_state, lt::session& ses);
--- examples/client_test_piece_cache_torrent.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <libtorrent/torrent_handle.hpp>
-#include <libtorrent/add_torrent_params.hpp>
-
-bool add_torrent(lt::session& ses, std::string torrent);
